238478bb9df9758a4e513c867e7bd3a6047a63b7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-17 14:28:36 +1100

    SequenceParserToken.replaceValue formatting

--- src/main/java/walkingkooka/text/cursor/parser/SequenceParserToken.java
+++ src/main/java/walkingkooka/text/cursor/parser/SequenceParserToken.java
@@ -41,7 +41,8 @@ public final class SequenceParserToken extends RepeatedOrSequenceParserToken {
         super(tokens, text);
     }
 
-    @Override SequenceParserToken replaceValue(final List<ParserToken> value) {
+    @Override //
+    SequenceParserToken replaceValue(final List<ParserToken> value) {
         return new SequenceParserToken(value, this.text());
     }
 
