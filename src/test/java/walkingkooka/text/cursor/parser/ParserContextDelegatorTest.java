650506a0e7fb26576eee1f2d96270360c1aa73a0
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-13 13:47:31 +1100

    ParserContextDelegator

---
+++ src/test/java/walkingkooka/text/cursor/parser/ParserContextDelegatorTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.text.cursor.parser;
+
+import walkingkooka.datetime.DateTimeContexts;
+import walkingkooka.math.DecimalNumberContext;
+import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.text.cursor.parser.ParserContextDelegatorTest.TestParserContextDelegator;
+
+import java.math.MathContext;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+public final class ParserContextDelegatorTest implements ParserContextTesting<TestParserContextDelegator> {
+
+
+    @Override
+    public TestParserContextDelegator createContext() {
+        return new TestParserContextDelegator();
+    }
+
+    private final static DecimalNumberContext DECIMAL_NUMBER_CONTEXT = DecimalNumberContexts.american(MathContext.DECIMAL32);
+
+    @Override
+    public String currencySymbol() {
+        return DECIMAL_NUMBER_CONTEXT.currencySymbol();
+    }
+
+    @Override
+    public char decimalSeparator() {
+        return DECIMAL_NUMBER_CONTEXT.decimalSeparator();
+    }
+
+    @Override
+    public String exponentSymbol() {
+        return DECIMAL_NUMBER_CONTEXT.exponentSymbol();
+    }
+
+    @Override
+    public char groupSeparator() {
+        return DECIMAL_NUMBER_CONTEXT.groupSeparator();
+    }
+
+    @Override
+    public MathContext mathContext() {
+        return DECIMAL_NUMBER_CONTEXT.mathContext();
+    }
+
+    @Override
+    public char negativeSign() {
+        return DECIMAL_NUMBER_CONTEXT.negativeSign();
+    }
+
+    @Override
+    public char percentageSymbol() {
+        return DECIMAL_NUMBER_CONTEXT.percentageSymbol();
+    }
+
+    @Override
+    public char positiveSign() {
+        return DECIMAL_NUMBER_CONTEXT.positiveSign();
+    }
+
+    static final class TestParserContextDelegator implements ParserContextDelegator {
+        @Override
+        public ParserContext parserContext() {
+            return ParserContexts.basic(
+                    DateTimeContexts.locale(
+                            Locale.ENGLISH,
+                            1950, // defaultYear
+                            50, // twoDigitYear
+                            LocalDateTime::now
+                    ),
+                    DECIMAL_NUMBER_CONTEXT
+            );
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TestParserContextDelegator> type() {
+        return TestParserContextDelegator.class;
+    }
+
+    @Override
+    public void testTypeNaming() {
+        throw new UnsupportedOperationException();
+    }
+}
