2d78ccd0030b387280a4ab78735036ec79a46650
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-16 10:17:07 +1100

    TransformingParserTest cleanup

--- src/test/java/walkingkooka/text/cursor/parser/TransformingParserTest.java
+++ src/test/java/walkingkooka/text/cursor/parser/TransformingParserTest.java
@@ -37,37 +37,110 @@ public class TransformingParserTest extends ParserWrapperTestCase<TransformingPa
 
     @Test
     public void testWithNullTransformerFails() {
-        assertThrows(NullPointerException.class, () -> TransformingParser.with(WRAPPED_PARSER, null));
+        assertThrows(
+                NullPointerException.class,
+                () -> TransformingParser.with(WRAPPED_PARSER, null)
+        );
     }
 
     @Test
-    public void testFailure() {
+    public void testParseFailure() {
         this.parseFailAndCheck("a");
     }
 
     @Test
-    public void testFailure2() {
+    public void testParseFailure2() {
         this.parseFailAndCheck("abc");
     }
 
     @Test
-    public void testSuccessEoc() {
-        this.parseAndCheck2("1", 1, "1", "");
+    public void testParseSuccessEoc() {
+        this.parseAndCheck2(
+                "1",
+                1,
+                "1",
+                ""
+        );
     }
 
     @Test
-    public void testSuccessEoc2() {
-        this.parseAndCheck2("123", 123, "123", "");
+    public void testParseSuccessEoc2() {
+        this.parseAndCheck2(
+                "123",
+                123,
+                "123",
+                ""
+        );
     }
 
     @Test
-    public void testSuccessTokenAfter() {
-        this.parseAndCheck2("123abc", 123, "123", "abc");
+    public void testParseSuccessTokenAfter() {
+        this.parseAndCheck2(
+                "123abc",
+                123,
+                "123",
+                "abc"
+        );
     }
 
     @Test
     public void testDefaultMethodTransform() {
         this.parseAndCheck4(WRAPPED_PARSER.transform(TRANSFORMER), "123abc", 123, "123", "abc");
+    }
+
+    // parse............................................................................................................
+
+    @Override
+    TransformingParser<ParserContext> createParser(final Parser<ParserContext> parser) {
+        return TransformingParser.with(
+                parser,
+                TRANSFORMER
+        );
+    }
+
+    @Override
+    Parser<ParserContext> wrappedParser() {
+        return WRAPPED_PARSER;
+    }
+
+    private TextCursor parseAndCheck2(final String in,
+                                      final long value,
+                                      final String text,
+                                      final String textAfter) {
+        return this.parseAndCheck3(
+                in,
+                value,
+                text,
+                textAfter
+        );
+    }
+
+    private TextCursor parseAndCheck3(final String from,
+                                      final long value,
+                                      final String text,
+                                      final String textAfter) {
+        return this.parseAndCheck4(
+                this.createParser(),
+                from,
+                value,
+                text,
+                textAfter
+        );
+    }
+
+    private TextCursor parseAndCheck4(final Parser<ParserContext> parser,
+                                      final String from,
+                                      final long value,
+                                      final String text,
+                                      final String textAfter) {
+        return this.parseAndCheck(
+                parser,
+                this.createContext(),
+                TextCursors.charSequence(from),
+                ParserTokens.bigInteger(BigInteger.valueOf(value), text),
+                text,
+                textAfter
+        );
     }
 
     // equals/hashCode..................................................................................................
@@ -110,43 +183,10 @@ public class TransformingParserTest extends ParserWrapperTestCase<TransformingPa
 
     @Test
     public void testToString() {
-        this.toStringAndCheck(this.createParser(), WRAPPED_PARSER.toString());
-    }
-
-    // parse............................................................................................................
-
-    @Override
-    TransformingParser<ParserContext> createParser(final Parser<ParserContext> parser) {
-        return TransformingParser.with(
-                parser,
-                TRANSFORMER
+        this.toStringAndCheck(
+                this.createParser(),
+                WRAPPED_PARSER.toString()
         );
-    }
-
-    @Override
-    Parser<ParserContext> wrappedParser() {
-        return WRAPPED_PARSER;
-    }
-
-    private TextCursor parseAndCheck2(final String in, final long value, final String text, final String textAfter) {
-        return this.parseAndCheck3(in, value, text, textAfter);
-    }
-
-    private TextCursor parseAndCheck3(final String from, final long value, final String text, final String textAfter) {
-        return this.parseAndCheck4(this.createParser(),
-                from,
-                value,
-                text,
-                textAfter);
-    }
-
-    private TextCursor parseAndCheck4(final Parser<ParserContext> parser, final String from, final long value, final String text, final String textAfter) {
-        return this.parseAndCheck(parser,
-                this.createContext(),
-                TextCursors.charSequence(from),
-                ParserTokens.bigInteger(BigInteger.valueOf(value), text),
-                text,
-                textAfter);
     }
 
     // Class............................................................................................................
