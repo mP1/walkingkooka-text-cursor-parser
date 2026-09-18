9c0c7c49e623a4bf84f61390511446f5b4695572
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 15:17:06 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/text/cursor/parser/ParsersTest.java
+++ src/test/java/walkingkooka/text/cursor/parser/ParsersTest.java
@@ -17,7 +17,6 @@
 package walkingkooka.text.cursor.parser;
 
 import org.junit.jupiter.api.Test;
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
@@ -38,10 +37,5 @@ public final class ParsersTest implements PublicStaticHelperTesting<Parsers> {
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 }
